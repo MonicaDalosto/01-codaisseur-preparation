c99fd5a9d4f03f7daa6be856a793ee39798e454d
MonicaDalosto <monica.kerber@gmail.com>
2022-05-25 22:53:40 +0200

    the teachers exercise

---
+++ week2/day3/lifting_state/src/App.js
@@ -0,0 +1,52 @@
+// import logo from "./logo.svg";
+
+// import React, { useState, useEffect } from "react";
+
+// /*
+//  node_modules/react/index.js
+
+//  export default React
+//  export {useState, useEffect}
+// */
+
+import users from './data/users.json';
+import './App.css';
+import { useEffect, useState } from 'react';
+import { Users } from './components/Users';
+import Search from './components/Search';
+
+function App() {
+  const [search, setSearch] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(users);
+
+  const handleChange = e => {
+    // e === event
+    /*
+        event = {
+          ...,
+          target: {
+            type: input,
+            value: 
+          }
+        }
+
+    */
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    // take the search input
+    // check if the search input is part of the user's name
+    const filtered = users.filter(user => user.name.includes(search));
+    setFilteredUsers(filtered);
+  }, [search]);
+
+  return (
+    <div className="App">
+      <Search search={search} setSearch={setSearch} />
+      <Users filteredUsers={filteredUsers} />
+    </div>
+  );
+}
+
+export default App;
